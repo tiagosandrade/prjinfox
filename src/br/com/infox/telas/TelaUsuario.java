cc12bee73609b4506c3b885dfa38a4fb12d97356
tiago <tiago@DESKTOP-FG914C5>
2017-03-02 09:04:38 -0300

    Testando Commit; Tela Usuario; Alteração de texto;

--- src/br/com/infox/telas/TelaUsuario.java
+++ src/br/com/infox/telas/TelaUsuario.java
@@ -48,8 +48,8 @@ public class TelaUsuario extends javax.swing.JInternalFrame {
                 cboUsuPerfil.setSelectedItem(rs.getString(6));
 
             } else {
-
-                JOptionPane.showMessageDialog(null, "//Usuário não cadastrado no banco de dados!");
+//Voltei o texto como estava originalmente e adicionei este comentário.
+                JOptionPane.showMessageDialog(null, "Usuário não cadastrado no banco de dados!");
 
                 txtUsuNome.setText(null);
                 txtUsuFone.setText(null);
